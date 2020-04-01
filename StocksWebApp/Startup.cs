a61a420c945c2a5ff3b223ba0ed7c9a89758c4c8
Paul Li <paul.j.li.97@gmail.com>
2020-04-01 17:10:09 -0400

    Cleaning up.

--- StocksWebApp/Startup.cs
+++ StocksWebApp/Startup.cs
@@ -49,7 +49,7 @@ namespace StocksWebApp
                 // Set the comments path for the Swagger JSON and UI.
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
+                //c.IncludeXmlComments(xmlPath);
             });
         }
 
