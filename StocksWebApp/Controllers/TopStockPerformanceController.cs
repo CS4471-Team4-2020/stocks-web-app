eab780ec2f8733c44cdc7865750e702a7389b48e
Paul Li <paul.j.li.97@gmail.com>
2020-04-02 02:45:43 -0400

    Added topstock page.

--- StocksWebApp/Controllers/TopStockPerformanceController.cs
+++ StocksWebApp/Controllers/TopStockPerformanceController.cs
@@ -1,81 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using StocksWebApp.Models;
 
 namespace StocksWebApp.Controllers
 {
     public class TopStockPerformanceController : Controller
     {
+        private static readonly HttpClient client = new HttpClient();
+        private static readonly Dictionary<string, string> kpiDict = new Dictionary<string, string>()
+        {
+            { "closing", "Highest Closing Price" },
+            { "high", "Highest High Price"},
+            { "low", "Lowest Low Price"},
+            { "market cap","Highest Market Capital"},
+            { "price to earn ratio", "Lowest Price to Earning Ratio" }
+        };
+
         public IActionResult Index()
         {
-            var topStockPerformanceViewModel = generateDummyData();
 
             //Default filter values
-            topStockPerformanceViewModel.kpi = topStockPerformanceViewModel.kpiDict.Keys.FirstOrDefault();
-            topStockPerformanceViewModel.timePeriod = 30;
+            string kpi = kpiDict.Keys.FirstOrDefault();
+            int days = 30;
+
+            //Get dates in the specified time period
+            var endDateTime = DateTime.ParseExact("08/12/2011 00:00:00", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);  //Latest end date noted in the database
+            var startDateTime = endDateTime.AddDays(-days);
+
+            //Query function for list of companies in specified order
+            var topStockPerformanceViewModel = GetContentAsync(kpi, startDateTime, endDateTime).Result;
+
+            //Fill out the remaining ViewModel fields
+            topStockPerformanceViewModel.kpi = kpi;
+            topStockPerformanceViewModel.timePeriod = days;
+            topStockPerformanceViewModel.kpiDict = kpiDict;
 
             return View("index", topStockPerformanceViewModel);
         }
         public IActionResult Filter()
         {
+
+            //Filter values
             var kpiSM = Request.Form["kpiSelectMenu"];
-            var tp = int.Parse(Request.Form["timePeriod"]);
+            var days = int.Parse(Request.Form["timePeriod"]);
 
-            var topStockPerformanceViewModel = generateDummyData();
+            //Generate the time period specified by filter
+            var endDateTime = DateTime.ParseExact("08/12/2011 00:00:00", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);  //Latest end date noted in the database
+            var startDateTime = endDateTime.AddDays(-days);
 
-            //Filters
+            //Query function for list of companies in specified order
+            var topStockPerformanceViewModel = GetContentAsync(kpiSM, startDateTime, endDateTime).Result;
+
+            //Fill out the remaining ViewModel fields
             topStockPerformanceViewModel.kpi = kpiSM;
-            topStockPerformanceViewModel.timePeriod = tp;
-
-            //TODO UPDATE THE RESULTS!
-            topStockPerformanceViewModel.companiesKPI.First().rank = 99;
+            topStockPerformanceViewModel.timePeriod = days;
+            topStockPerformanceViewModel.kpiDict = kpiDict;
 
             return View("index", topStockPerformanceViewModel);
         }
 
 
-        private TopStockPerformanceViewModel generateDummyData() {
-            var dummyData = new TopStockPerformanceViewModel();
+        public async Task<TopStockPerformanceViewModel> GetContentAsync(string kpi, DateTime start, DateTime end)
+        {
 
+            var requestData = new PerformanceRequestData()
+            {
+                Kpi = kpi,
+                StartDate = start,
+                EndDate = end
 
-
-            dummyData.kpiDict = new Dictionary<string, string>();
-            dummyData.kpiDict["Big1"] = "WOWZA1";
-            dummyData.kpiDict["Big2"] = "WOWZA2";
-
-            dummyData.timePeriod = 30; // 30 days
-
-            dummyData.companiesKPI = new List<KPIInfo>();
-            dummyData.companiesKPI.Add(
-                new KPIInfo()
-                {
-                    rank = 1,
-                    companyAbriv = "ABC",
-                    companyName = "ABC-Company",
-                    value = 5345.54
-                });
-            dummyData.companiesKPI.Add(
-                new KPIInfo()
-                {
-                    rank = 2,
-                    companyAbriv = "DEF",
-                    companyName = "DEF-Company",
-                    value = 45.54
-                });
-            dummyData.companiesKPI.Add(
-                new KPIInfo()
-                {
-                    rank = 3,
-                    companyAbriv = "GHI",
-                    companyName = "GHI-Company",
-                    value = 56.66
-                });
-
-
-            return dummyData;
+            };
+            var content = new StringContent(JsonConvert.SerializeObject(requestData), Encoding.UTF8, "application/json");
+            //var azureFuncString = "http://localhost:7071/api/QueryData";
+            var azureFuncString = "https://cs4471-topstockperformerservice.azurewebsites.net/api/Function1";
+            var response = await client.PostAsync(azureFuncString, content);
+            var responseObj = await response.Content.ReadAsAsync<TopStockPerformanceViewModel>();
+            return responseObj;
         }
+
+        private class PerformanceRequestData
+        {
+            public string Kpi { get; set; }
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+        }
+
     }
-}
